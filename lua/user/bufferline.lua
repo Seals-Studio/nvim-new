616d13bac881e2ec6598d2b0245ce9a6f680eefc
silianpan <liu.pan@silianpan.cn>
2024-07-21 21:23:59 +0800

    feat: 新增scope插件，增强选项卡

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -1,6 +1,6 @@
 
 local M = {
-  'akinsho/bufferline.nvim',
+  "akinsho/bufferline.nvim",
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons'
 }
---
+++ lua/user/scope.lua
@@ -0,0 +1,9 @@
+local M = {
+  "tiagovla/scope.nvim"
+}
+
+function M.config()
+  require("scope").setup({})
+end
+
+return M
