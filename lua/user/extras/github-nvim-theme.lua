77cabcbfd6ebd3852049709a09b5d6350d52b4f2
silianpan <liu.pan@silianpan.cn>
2024-07-22 22:26:27 +0800

    fix: 优化github主题

--- lua/user/extras/github-nvim-theme.lua
+++ lua/user/extras/github-nvim-theme.lua
@@ -1,10 +1,16 @@
 local M = {
-  "projekt0n/github-nvim-theme"
+  "projekt0n/github-nvim-theme",
 }
 
 function M.config()
   require('github-theme').setup({})
-  vim.cmd('colorscheme github_dark_tritanopia')
+  -- github_dark
+  -- github_dark_default
+  -- github_dark_dimmed
+  -- github_dark_high_contrast
+  -- github_dark_colorblind
+  -- github_dark_tritanopia
+  vim.cmd('colorscheme github_dark_dimmed')
 end
 
 return M
