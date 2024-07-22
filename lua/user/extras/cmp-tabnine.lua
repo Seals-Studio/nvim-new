d8f5ec5073eaf982b6361fbd8b393206937a34c5
liupan <1206284818@qq.com>
2024-07-22 11:49:05 +0800

    fix: windows terminal 编译支持

--- lua/user/extras/cmp-tabnine.lua
+++ lua/user/extras/cmp-tabnine.lua
@@ -2,6 +2,8 @@ local M = {
     "tzachar/cmp-tabnine",
     event = "InsertEnter",
     build = "./install.sh",
+    -- windows terminal support
+    -- build = "powershell ./install.ps1",
 }
 
 function M.config() end
