791bd8f034791022a37736041ec67a4563960446
liupan <1206284818@qq.com>
2024-07-23 17:27:53 +0800

    fix: 优化bufferline快捷键

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -41,18 +41,72 @@ function M.config()
   local opts = { noremap = true, silent = true }
   local keymap = vim.api.nvim_set_keymap
 
-  keymap("n", "<leader>1", ":BufferLineGoToBuffer 1<CR>", opts)
-  keymap("n", "<leader>2", ":BufferLineGoToBuffer 2<CR>", opts)
-  keymap("n", "<leader>3", ":BufferLineGoToBuffer 3<CR>", opts)
-  keymap("n", "<leader>4", ":BufferLineGoToBuffer 4<CR>", opts)
-  keymap("n", "<leader>5", ":BufferLineGoToBuffer 5<CR>", opts)
-  keymap("n", "<leader>6", ":BufferLineGoToBuffer 6<CR>", opts)
-  keymap("n", "<leader>7", ":BufferLineGoToBuffer 7<CR>", opts)
-  keymap("n", "<leader>8", ":BufferLineGoToBuffer 8<CR>", opts)
-  keymap("n", "<leader>9", ":BufferLineGoToBuffer 9<CR>", opts)
-  keymap("n", "<leader>bp", ":BufferLinePick<CR>", opts)
+  -- keymap("n", "<leader>1", ":BufferLineGoToBuffer 1<CR>", opts)
+  -- keymap("n", "<leader>2", ":BufferLineGoToBuffer 2<CR>", opts)
+  -- keymap("n", "<leader>3", ":BufferLineGoToBuffer 3<CR>", opts)
+  -- keymap("n", "<leader>4", ":BufferLineGoToBuffer 4<CR>", opts)
+  -- keymap("n", "<leader>5", ":BufferLineGoToBuffer 5<CR>", opts)
+  -- keymap("n", "<leader>6", ":BufferLineGoToBuffer 6<CR>", opts)
+  -- keymap("n", "<leader>7", ":BufferLineGoToBuffer 7<CR>", opts)
+  -- keymap("n", "<leader>8", ":BufferLineGoToBuffer 8<CR>", opts)
+  -- keymap("n", "<leader>9", ":BufferLineGoToBuffer 9<CR>", opts)
+  -- keymap("n", "<leader>bp", ":BufferLinePick<CR>", opts)
   keymap("n", "<C-h>", ":BufferLineCyclePrev<CR>", opts)
   keymap("n", "<C-l>", ":BufferLineCycleNext<CR>", opts)
+
+  local wk = require "which-key"
+  wk.add {
+    {
+      "<leader>1",
+      "<cmd>:BufferLineGoToBuffer 1<CR>",
+      desc = "Tab 1",
+    },
+    {
+      "<leader>2",
+      "<cmd>:BufferLineGoToBuffer 2<CR>",
+      desc = "Tab 2",
+    },
+    {
+      "<leader>3",
+      "<cmd>:BufferLineGoToBuffer 3<CR>",
+      desc = "Tab 3",
+    },
+    {
+      "<leader>4",
+      "<cmd>:BufferLineGoToBuffer 4<CR>",
+      desc = "Tab 4",
+    },
+    {
+      "<leader>5",
+      "<cmd>:BufferLineGoToBuffer 5<CR>",
+      desc = "Tab 5",
+    },
+    {
+      "<leader>6",
+      "<cmd>:BufferLineGoToBuffer 6<CR>",
+      desc = "Tab 6",
+    },
+    {
+      "<leader>7",
+      "<cmd>:BufferLineGoToBuffer 7<CR>",
+      desc = "Tab 7",
+    },
+    {
+      "<leader>8",
+      "<cmd>:BufferLineGoToBuffer 8<CR>",
+      desc = "Tab 8",
+    },
+    {
+      "<leader>9",
+      "<cmd>:BufferLineGoToBuffer 9<CR>",
+      desc = "Tab 9",
+    },
+    {
+      "<leader>bp",
+      "<cmd>:BufferLinePick<CR>",
+      desc = "Tab Pick",
+    },
+  }
 end
 
 return M
