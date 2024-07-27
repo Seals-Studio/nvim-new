52421694b60630e4a5f1a2386aae29b3696d6207
silianpan <liu.pan@silianpan.cn>
2024-07-27 11:56:44 +0800

    fix: 选用github主题

--- lua/user/extras/github-nvim-theme.lua
+++ lua/user/extras/github-nvim-theme.lua
@@ -3,14 +3,23 @@ local M = {
 }
 
 function M.config()
-  require('github-theme').setup({})
+  require("github-theme").setup {
+    options = {
+      transparent = true,
+    },
+  }
   -- github_dark
-  -- github_dark_default
   -- github_dark_dimmed
+  -- github_dark_default
   -- github_dark_high_contrast
   -- github_dark_colorblind
   -- github_dark_tritanopia
-  -- vim.cmd('colorscheme github_dark_dimmed')
+  -- github_light
+  -- github_light_default
+  -- github_light_high_contrast
+  -- github_light_colorblind
+  -- github_light_tritanopia
+  vim.cmd "colorscheme github_dark_dimmed"
 end
 
 return M
--- lua/user/extras/tokyonight.lua
+++ lua/user/extras/tokyonight.lua
@@ -6,7 +6,7 @@ local M = {
 }
 
 function M.config()
-  vim.cmd.colorscheme "tokyonight-night"
+  -- vim.cmd.colorscheme "tokyonight-night"
 end
 
 return M
