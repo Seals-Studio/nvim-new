fda78a0ff334f9c08b0a8e9c875c527be805a64e
silianpan <liu.pan@silianpan.cn>
2024-07-29 22:32:19 +0800

    fix: 优化jdtls运行参数，添加lombok

--- lua/user/lspsettings/jdtls.lua
+++ lua/user/lspsettings/jdtls.lua
@@ -1,7 +1,8 @@
 return {
   cmd = {
-    'jdtls',
-    '--java-executable',
-    '/Users/liupan/work/tools/dev/jdk-17.0.8.jdk/Contents/Home/bin/java'
-  }
+    "jdtls",
+    "--java-executable",
+    "/Users/liupan/work/tools/dev/jdk-17.0.8.jdk/Contents/Home/bin/java",
+    "--jvm-arg=-javaagent:/Users/liupan/work/tools/neovim-plugins/lombok.jar",
+  },
 }
